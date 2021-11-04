9ba82958c48f6cfd3a516a0695a730b6c61a4ef1
Felix <felix.gaillard51@gmail.com>
2021-11-04 11:47:38 +0100

    fin tp3

---
+++ jdbc/src/main/java/fr/diginamic/jdbc/TestInsertion.java
@@ -0,0 +1,33 @@
+package fr.diginamic.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ResourceBundle;
+
+public class TestInsertion {
+
+	private static String DB_URL;
+	private static String DB_LOGIN;
+	private static String DB_PWD;
+
+	public static void main(String[] args) throws SQLException {
+
+//		Connection à la BDD
+		ResourceBundle props = ResourceBundle.getBundle("database");
+		DB_URL = props.getString("jdbc.url");
+		DB_LOGIN = props.getString("jdbc.login");
+		DB_PWD = props.getString("jdbc.pwd");
+		Connection connection = DriverManager.getConnection(DB_URL, DB_LOGIN, DB_PWD);
+		System.out.println(connection);
+		
+//		INSERER UN NOUVEAU FOURNISSEUR
+		Statement insererFournisseur = connection.createStatement();
+		int nb = insererFournisseur.executeUpdate("INSERT INTO FOURNISSEUR (ID,NOM) VALUES (6,'Test')");	
+		
+		insererFournisseur.close();
+		connection.close();
+
+	}
+}
---
+++ jdbc/src/main/java/fr/diginamic/jdbc/TestUpdate.java
@@ -0,0 +1,32 @@
+package fr.diginamic.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ResourceBundle;
+
+public class TestUpdate {
+
+	private static String DB_URL;
+	private static String DB_LOGIN;
+	private static String DB_PWD;
+
+	public static void main(String[] args) throws SQLException {
+
+//		Connection à la BDD
+		ResourceBundle props = ResourceBundle.getBundle("database");
+		DB_URL = props.getString("jdbc.url");
+		DB_LOGIN = props.getString("jdbc.login");
+		DB_PWD = props.getString("jdbc.pwd");
+		Connection connection = DriverManager.getConnection(DB_URL, DB_LOGIN, DB_PWD);
+		System.out.println(connection);
+
+//		MODIFIER UN FOURNISSEUR
+		Statement modifierFournisseur = connection.createStatement();
+		int nb = modifierFournisseur.executeUpdate("UPDATE FOURNISSEUR SET NOM='La Maison des Peintures' WHERE ID=4");
+
+		modifierFournisseur.close();
+		connection.close();
+	}
+}
